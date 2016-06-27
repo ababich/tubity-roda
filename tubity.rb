6ebe92dcfed910c4388d1f8f5f743880d08be523
Alexey Babich <ababich@productengine.com>
2016-06-27 15:22:31 +0300

    Convert to roda’s normal JSON processor

--- tubity.rb
+++ tubity.rb
@@ -1,21 +1,22 @@
 require 'roda'
+require 'byebug'
 require 'oj'
 
 Dir[File.dirname(__FILE__) + '/models/*.rb'].each {|file| require file }
 
 class Tubity < Roda
+  plugin :json_parser, parser: Oj.method(:load)
+  plugin :json, serializer: proc{|o| Oj.dump(o)}
+
   route do |r|
     r.post 's' do
-      r.body.rewind  # in case someone already read it
-      data = Oj.load(r.body.read)
-      url = data['url']
+      url = r.params['url']
       shorten_url = String.new(TubityUrl.shorten(url: url).to_s)
 
-      # it is important for Oj not to use new ruby hash syntax
-      Oj.dump({
+      {
         'url' => url,
         'shorten_url' => shorten_url
-      })
+      }
     end
 
     r.is ':hash' do |hash|
